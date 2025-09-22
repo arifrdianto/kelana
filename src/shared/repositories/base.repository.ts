141e8943260ac78f06439fca88c877cd70742aff
Arif Rudianto <arifrdianto@gmail.com>
2025-09-22 10:16:14 +0700

    refactor: base repository

--- src/shared/repositories/base.repository.ts
+++ src/shared/repositories/base.repository.ts
@@ -1,24 +1,44 @@
+import { NotFoundException } from '@nestjs/common';
+
 import {
   DeepPartial,
   DeleteResult,
+  EntityManager,
   FindManyOptions,
   FindOptionsWhere,
+  InsertResult,
+  RemoveOptions,
   Repository,
   UpdateResult,
 } from 'typeorm';
-import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity.js';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 import { BaseEntity } from '@/database/entities/base.entity';
 
 export abstract class BaseRepository<T extends BaseEntity> {
   constructor(protected readonly repository: Repository<T>) {}
 
-  async findOneById(id: string): Promise<T | null> {
-    return this.repository.findOneBy({ id } as FindOptionsWhere<T>);
+  async findOneById(id: string, relations: string[] = []): Promise<T | null> {
+    return this.repository.findOne({
+      where: { id } as FindOptionsWhere<T>,
+      relations,
+    });
+  }
+
+  async findOneByIdOrFail(id: string, relations: string[] = []): Promise<T> {
+    const entity = await this.findOneById(id, relations);
+    if (!entity) throw new NotFoundException(`Entity with id "${id}" not found`);
+    return entity;
   }
 
   async findOneBy(where: FindOptionsWhere<T>): Promise<T | null> {
     return this.repository.findOneBy(where);
+  }
+
+  async findOneByOrFail(where: FindOptionsWhere<T>): Promise<T> {
+    const entity = await this.findOneBy(where);
+    if (!entity) throw new NotFoundException(`Entity not found`);
+    return entity;
   }
 
   async find(options?: FindManyOptions<T>): Promise<T[]> {
@@ -30,13 +50,25 @@ export abstract class BaseRepository<T extends BaseEntity> {
   }
 
   async create(entity: DeepPartial<T>): Promise<T> {
-    const newEntity = this.repository.create(entity);
-    return this.repository.save(newEntity);
+    return this.repository.save(this.repository.create(entity));
   }
 
   async createMany(entities: DeepPartial<T>[]): Promise<T[]> {
-    const newEntities = this.repository.create(entities);
-    return this.repository.save(newEntities);
+    return this.repository.save(this.repository.create(entities));
+  }
+
+  async insert(entities: QueryDeepPartialEntity<T>[]): Promise<InsertResult> {
+    return this.repository.insert(entities);
+  }
+
+  async save<E extends DeepPartial<T>>(entity: E): Promise<T>;
+  async save<E extends DeepPartial<T>>(entities: E[]): Promise<T[]>;
+  async save<E extends DeepPartial<T>>(entityOrEntities: E | E[]): Promise<T | T[]> {
+    if (Array.isArray(entityOrEntities)) {
+      return (await this.repository.save(entityOrEntities as DeepPartial<T>[])) as unknown as T[];
+    } else {
+      return (await this.repository.save(entityOrEntities)) as unknown as T;
+    }
   }
 
   async update(
@@ -46,24 +78,16 @@ export abstract class BaseRepository<T extends BaseEntity> {
     return this.repository.update(criteria, partialEntity);
   }
 
-  async updateById(id: string, partialEntity: QueryDeepPartialEntity<T>): Promise<UpdateResult> {
-    return this.repository.update(id, partialEntity);
-  }
-
   async delete(criteria: string | string[] | FindOptionsWhere<T>): Promise<DeleteResult> {
     return this.repository.delete(criteria);
-  }
-
-  async deleteById(id: string): Promise<DeleteResult> {
-    return this.repository.delete(id);
   }
 
   async softDelete(criteria: string | string[] | FindOptionsWhere<T>): Promise<UpdateResult> {
     return this.repository.softDelete(criteria);
   }
 
-  async softDeleteById(id: string): Promise<UpdateResult> {
-    return this.repository.softDelete(id);
+  async restore(criteria: string | string[] | FindOptionsWhere<T>): Promise<UpdateResult> {
+    return this.repository.restore(criteria);
   }
 
   async count(options?: FindManyOptions<T>): Promise<number> {
@@ -82,19 +106,21 @@ export abstract class BaseRepository<T extends BaseEntity> {
     return this.repository.existsBy(where);
   }
 
-  async save(entity: DeepPartial<T>): Promise<T>;
-  async save(entities: DeepPartial<T>[]): Promise<T[]>;
-  async save(entityOrEntities: DeepPartial<T> | DeepPartial<T>[]): Promise<T | T[]> {
-    return Array.isArray(entityOrEntities)
-      ? this.repository.save(entityOrEntities)
-      : this.repository.save(entityOrEntities);
+  async remove(entity: T, options?: RemoveOptions): Promise<T>;
+  async remove(entities: T[], options?: RemoveOptions): Promise<T[]>;
+  async remove(entityOrEntities: T | T[], options?: RemoveOptions): Promise<T | T[]> {
+    if (Array.isArray(entityOrEntities)) {
+      return this.repository.remove(entityOrEntities, options);
+    } else {
+      return this.repository.remove(entityOrEntities, options);
+    }
   }
 
-  async remove(entity: T): Promise<T>;
-  async remove(entities: T[]): Promise<T[]>;
-  async remove(entityOrEntities: T | T[]): Promise<T | T[]> {
-    return Array.isArray(entityOrEntities)
-      ? this.repository.remove(entityOrEntities)
-      : this.repository.remove(entityOrEntities);
+  async withTransaction<R>(
+    manager: EntityManager,
+    fn: (transactionalRepo: Repository<T>) => Promise<R>,
+  ): Promise<R> {
+    const transactionalRepo = manager.getRepository(this.repository.target);
+    return fn(transactionalRepo);
   }
 }
